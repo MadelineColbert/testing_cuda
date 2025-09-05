84035b531b89223ec1c61ae069eb8050d212da7d
MadelineColbert <madeline.r.colbert+eglrsaf4@gmail.com>
2025-09-05 12:52:47 -0500

    Saving current cusolver changes

---
+++ testing_cusolver/include/common.h
@@ -0,0 +1,35 @@
+#ifndef CYTHON_CUDA_SAMPLES_COMMON_H
+#define CYTHON_CUDA_SAMPLES_COMMON_H
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+#include <iostream>
+#include <cusolverDn.h>
+
+
+
+static void CudaCheck(cudaError_t error, const char *file, int line) {
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Error: %s:%d, ", file, line);
+        fprintf(stderr, "code: %d, reason: %s\n", error,
+                cudaGetErrorString(error));
+        exit( EXIT_FAILURE );
+    }
+}
+
+
+#define CUDA_CHECK( err ) (CudaCheck( err, __FILE__, __LINE__ ))
+
+
+static void CusolverCheck(cusolverStatus_t error, const char *file, int line) {
+    if (error != CUSOLVER_STATUS_SUCCESS) {
+        fprintf(stderr, "Error: %s:%d, ", file, line);
+        fprintf(stderr, "code: %d, reason: %d\n", error,
+                "Not Working");
+        exit( EXIT_FAILURE );
+    }
+}
+
+#define CUSOLVER_CHECK( err ) (CusolverCheck(err, __FILE__, __LINE__))
+
+#endif
--- testing_cusolver/test.cu
+++ testing_cusolver/test.cu
@@ -3,34 +3,92 @@
 #include <cusolverDn.h>
 #include <vector>
 #include <cstdio>
+#include <random>
+#include "include/common.h"
 
-void cusolver_problem() {
+using data_type=double;
+
+struct csp_resp {
+    int m;
+    int n;
+    std::vector<data_type> h_A;
+    std::vector<data_type> workspace;
+    std::vector<int> devipiv;
+    int* devinfo;
+
+    csp_resp(int mv, int nv) 
+        : m(mv), n(nv),
+          h_A(mv*nv),
+          workspace(mv*nv),
+          devipiv(min(mv,nv)) {
+
+    }
+};
+
+void cusolver_problem(csp_resp *test_in) {
     cusolverDnHandle_t handle;
     cudaStream_t stream;
 
-    cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
-    cusolverDnCreate(&handle);
-    cusolverDnSetStream(handle, stream);
+    CUDA_CHECK(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
+    CUSOLVER_CHECK(cusolverDnCreate(&handle));
+    CUSOLVER_CHECK(cusolverDnSetStream(handle, stream));
 
-    double* A;
-    double* workspace;
-    int* devipiv;
-    int* devinfo;
+    int lda=max(1,test_in->m);
+
+    data_type *d_A = nullptr;
+    data_type *d_workspace = nullptr;
+    int *d_devipiv = nullptr;
+    int *d_devinfo = nullptr;
+
+    int malloc_size = test_in->m*test_in->n*sizeof(data_type);
+
+    CUDA_CHECK(cudaMalloc((void**)&d_A, malloc_size));
+    CUDA_CHECK(cudaMalloc((void**)&d_workspace,  malloc_size));
+    CUDA_CHECK(cudaMalloc((void**)&d_devipiv,  min(test_in->m,test_in->n)*sizeof(int)));
+    CUDA_CHECK(cudaMalloc((void**)&d_devinfo,  sizeof(int)));
+
+    CUDA_CHECK(cudaMemcpyAsync(d_A, test_in->h_A.data(), malloc_size, cudaMemcpyHostToDevice, stream));
+
+    CUSOLVER_CHECK(cusolverDnDgetrf(handle, test_in->m, test_in->n, d_A, lda, d_workspace, d_devipiv, d_devinfo));
 
 
-    int m=0;
-    int n=0;
-    int lda=0;
+    CUDA_CHECK(cudaMemcpyAsync(test_in->h_A.data(), d_A, malloc_size, cudaMemcpyDeviceToHost, stream));
+    CUDA_CHECK(cudaMemcpyAsync(test_in->workspace.data(), d_workspace, malloc_size, cudaMemcpyDeviceToHost, stream));
 
-    cusolverDnDgetrf(handle, m,n, A, lda, workspace, devipiv, devinfo);
+    CUDA_CHECK(cudaMemcpyAsync(test_in->devipiv.data(), d_devipiv, min(test_in->m,test_in->n)*sizeof(int), cudaMemcpyDeviceToHost, stream));
+    
+    // CUDA_CHECK(cudaMemcpyAsync(test_in->devinfo, d_devinfo, sizeof(int), cudaMemcpyDeviceToHost, stream));
 
-    cudaStreamSynchronize(stream);
+    CUDA_CHECK(cudaStreamSynchronize(stream));
 
-    cusolverDnDestroy(handle);
-    cudaStreamDestroy(stream);
+    CUDA_CHECK(cudaFree(d_A));
+    CUDA_CHECK(cudaFree(d_workspace));
+    CUDA_CHECK(cudaFree(d_devinfo));
+    CUDA_CHECK(cudaFree(d_devipiv));
+
+    CUSOLVER_CHECK(cusolverDnDestroy(handle));
+    CUDA_CHECK(cudaStreamDestroy(stream));
 
 }
 
+void generate_A(csp_resp *test_in){
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<> distrib(1,100);
+
+    for (int i=0; i<(test_in->m * test_in->n); i++){
+        int randomNumber = distrib(gen);
+        test_in->h_A[i] = (data_type)randomNumber;   
+    }
+}
+
 int main () {
-    printf("Testing Make");
+    csp_resp test_in(10,10);
+    generate_A(&test_in);
+    cusolver_problem(&test_in);
+    for (auto i : test_in.h_A) {
+        printf("%f ", i);
+    }
 }
